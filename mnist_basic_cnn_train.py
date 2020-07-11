ae5edef6a2e00009b82db28f919e556cbcf22844
Xuandong Xu <xuandong34@126.com>
2020-07-11 10:26:52 +0800

    updated train

--- mnist_basic_cnn_train.py
+++ mnist_basic_cnn_train.py
@@ -5,14 +5,16 @@ import tensorflow as tf
 mnist = mnist.MNIST()
 train_dataset, test_dataset = mnist.train_dataset, mnist.test_dataset
 
-cnn_model = cnn.basic_model()
+model = cnn.basic_model()
 
+# Define loss object/function & optimizer
 loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 
 # Prepare the training metrics
-train_acc_metric = tf.keras.metrics.SparseCategoricalCrossentropy()
-val_acc_metric = tf.keras.metrics.SparseCategoricalCrossentropy()
+train_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
+val_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy()
+epoch_loss_avg = tf.keras.metrics.Mean()
 
 def loss(model, x, y, training):
     y_ = model(x, training=training)
@@ -24,13 +26,19 @@ def train_step(model, x_batch, y_batch, training):
         y_, loss_value = loss(model, x_batch, y_batch, training)
     grads = tape.gradient(loss_value, model.trainable_weights)
     optimizer.apply_gradients(zip(grads, model.trainable_weights))
-    train_acc_metric.update_state(y_batch, y_)
+    train_acc_metric.update_state(y_batch, model(x_batch, training=True))
+    epoch_loss_avg.update_state(loss_value)
     return loss_value
 
 @tf.function
 def test_step(model, x_batch, y_batch):
     val_logits = model(x_batch, training=False)
     val_acc_metric.update_state(y_batch, val_logits)
+
+
+log_train_acc = []
+log_val_acc = []
+log_train_loss = []
 
 import time
 
@@ -40,7 +48,7 @@ for epoch in range(epochs):
     start_time = time.time()
 
     for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
-        loss_value = train_step(cnn_model, x_batch_train, y_batch_train, training=True)
+        loss_value = train_step(model, x_batch_train, y_batch_train, training=True)
 
         if step % 200 == 0:
             print("Training loss (for one batch) at step %d: %.4f" % (step, float(loss_value)))
@@ -50,14 +58,20 @@ for epoch in range(epochs):
     train_acc = train_acc_metric.result()
     print("Training acc over epoch: %.4f" % (float(train_acc),))
 
+    epoch_ave_loss = epoch_loss_avg.result()
+
     # Reset training metrics at the end of each epoch
     train_acc_metric.reset_states()
 
     # Validation
     for x_batch_val, y_batch_val in test_dataset:
-        test_step(cnn_model, x_batch_val, y_batch_val)
+        test_step(model, x_batch_val, y_batch_val)
     
     val_acc = val_acc_metric.result()
     val_acc_metric.reset_states()
     print("Validation acc: %.4f" % (float(val_acc),))
     print("Time taken: %.2fs" % (time.time() - start_time))
+
+    log_train_acc.append(train_acc)
+    log_val_acc.append(val_acc)
+    log_train_loss.append(epoch_ave_loss)
--- utility/train.py
+++ utility/train.py
@@ -1,5 +1,26 @@
 import tensorflow as tf
-from tensorflow import keras
 
-def train(model, dataset, config):
-    pass
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+
+# Prepare the training metrics
+train_acc_metric = tf.keras.metrics.SparseCategoricalCrossentropy()
+val_acc_metric = tf.keras.metrics.SparseCategoricalCrossentropy()
+
+def loss(model, x, y, training):
+    y_ = model(x, training=training)
+    return y_, loss_object(y_true=y, y_pred=y_)
+
+@tf.function
+def train_step(model, x_batch, y_batch, training):
+    with tf.GradientTape() as tape:
+        y_, loss_value = loss(model, x_batch, y_batch, training)
+    grads = tape.gradient(loss_value, model.trainable_weights)
+    optimizer.apply_gradients(zip(grads, model.trainable_weights))
+    train_acc_metric.update_state(y_batch, y_)
+    return loss_value
+
+@tf.function
+def test_step(model, x_batch, y_batch):
+    val_logits = model(x_batch, training=False)
+    val_acc_metric.update_state(y_batch, val_logits)
